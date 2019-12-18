b467a12b52afd5eed345591a2953c14c3e5c900f
Andrew Maddocks <andrewmadd@icloud.com>
2019-12-17 17:37:29 -0700

    finished MVP!

---
+++ friends/src/components/AddFriend.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const AddFriend = () => {
+  const [usersInfo, setUsersInfo] = useState({
+    name: "",
+    age: "",
+    email: ""
+  });
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/friends", usersInfo)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  };
+
+  const handleChanges = e => {
+    let name = e.target.name;
+
+    setUsersInfo({ ...usersInfo, [name]: e.target.value });
+  };
+  return (
+    <form onSubmit={handleSubmit} className="forms">
+      <label>Name</label>
+      <input
+        type="text"
+        name="name"
+        placeholder=""
+        value={usersInfo.name}
+        onChange={handleChanges}
+      />
+      <label>Age</label>
+      <input
+        type="text"
+        name="age"
+        placeholder=" "
+        value={usersInfo.age}
+        onChange={handleChanges}
+      />
+      <label>Email</label>
+      <input
+        type="text"
+        name="email"
+        placeholder=""
+        value={usersInfo.email}
+        onChange={handleChanges}
+      />
+      <div>
+        <button type="submit">Add Friend</button>
+      </div>
+    </form>
+  );
+};
+export default AddFriend;
---
+++ friends/src/components/Login.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const Login = props => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    let name = e.target.name;
+    setCredentials({ ...credentials, [name]: e.target.value });
+  };
+
+  const login = e => {
+    e.preventDefault();
+    console.log(credentials.username);
+    console.log(credentials.password);
+    console.log(credentials);
+    axiosWithAuth()
+      .post("/login", credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/privateFriends");
+      })
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
+//username: 'Lambda School', password: 'i<3Lambd4'
